bfc65180262ee3e2d2e2166c1d105cce0692c64f
eduardoadoglio <eduardoadoglio4@gmail.com>
2018-05-07 11:22:46 -0300

    Testezao

--- cadastro.php
+++ cadastro.php
@@ -6,12 +6,17 @@
 	<link rel="shortcut icon" href="">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<meta charset="utf-8">
 </head>
 <?php
 	include("conexao.php");
 ?>
 <body>
-
+<form method="post" action="validacaoCadastro.php">
+<input type="text" name="nmfuncionario" placeholder="Nome de usuário"><br>
+<input type="password" name="senhafuncionario" placeholder="Senha"> <br>
+<input type="submit" value="Enviar">
+</form>
 </body>
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
--- conexao.php
+++ conexao.php
@@ -2,11 +2,6 @@
 
 $conn = new mysqli('localhost', 'root', 'usbw');
 
-$sql = "INSERT INTO tb_funcionario VALUES (NULL, $nmfuncionario, $nmsenha)";
-
-
-
-
 if ($conn->connect_error) {
     echo("Connection failed: " . $conn->connect_error);
 } 
